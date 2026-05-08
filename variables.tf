65c3d6592f23a4482ceea2c24f7914ad34c5203d
milospejic <milospejic2001@gmail.com>
2026-05-08 10:04:35 +0200

    Initial terraform commit

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "gcp_region" {
+    description = "The region for the project"
+    type = string
+    default = "europe-west3"
+  }
+  
+variable "gcp_project" {
+    description = "The GCP project ID"
+    type = string
+    default = "sara-sandbox-interns"
+}
